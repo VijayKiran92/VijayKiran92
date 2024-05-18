1095a5218a9187267321fffcdf5a8c8adf71d859
VijayKiran92 <vijaykrishnakanthdeekonda108@gmaqil.com>
2024-05-18 15:43:46 +0530

    First Commit

---
+++ Practise.py
@@ -0,0 +1,12 @@
+l1 = [10, 20, 15, 12, 10, -56, 78, 4]
+print(l1,id(l1))
+
+l1.sort(reverse=True)
+print(l1)
+
+
+l1 = [10, 20, 15, 12, 10, -56, 78, 4]
+print(l1,id(l1))
+
+l1.sort(reverse=False)
+print(l1,id(l1))
